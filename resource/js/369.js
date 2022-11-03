b8a4732efe338e0922a63dfacdc372c6130a9ce1
jjy <jaeng12200@naver.com>
2022-11-03 17:51:36 +0900

    커미또

--- resource/js/369.js
+++ resource/js/369.js
@@ -3,5 +3,22 @@ console.log('js연결성공');
 const start = document.getElementById('startbtn');
 start.addEventListener('click', function(){
     console.log('이벤트리스너작동')
-    let submit = prompt('', "숫자를 입력하세요");
+    const submit = prompt('', "숫자를 입력하세요");
+    if(submit<0){
+        submit
+    }
+    
+
+    
+    for (let i = 1; i <= end; i++){
+
+    }
 })
+
+function game369(number){
+    let gagong = number%10;
+}
+
+
+
+
